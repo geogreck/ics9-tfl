b088c09af91aa5795e4a48e741f4ca76b0ac4f8a
Georgy Grechko <74151984+geogreck@users.noreply.github.com>
2024-02-11 02:14:09 +0300

    Revert "(lab 1_0): remove redundant config field"
    This reverts commit bdc8e232fe3cf95d64160d015e05493d0ac006ac.

--- lab1_0/config/config.go
+++ lab1_0/config/config.go
@@ -1,7 +1,8 @@
 package config
 
 type InputConfig struct {
-	N        int    `yaml:"n"`
-	Word     string `yaml:"word"`
-	RawRules string `yaml:"rules"`
+	N         int      `yaml:"n"`
+	Variables []string `yaml:"variables"`
+	Word      string   `yaml:"word"`
+	RawRules  string   `yaml:"rules"`
 }
