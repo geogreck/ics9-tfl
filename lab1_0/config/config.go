bdc8e232fe3cf95d64160d015e05493d0ac006ac
Georgy Grechko <74151984+geogreck@users.noreply.github.com>
2024-02-11 00:08:49 +0300

    (lab 1_0): remove redundant config field

--- lab1_0/config/config.go
+++ lab1_0/config/config.go
@@ -1,8 +1,7 @@
 package config
 
 type InputConfig struct {
-	N         int      `yaml:"n"`
-	Variables []string `yaml:"variables"`
-	Word      string   `yaml:"word"`
-	RawRules  string   `yaml:"rules"`
+	N        int    `yaml:"n"`
+	Word     string `yaml:"word"`
+	RawRules string `yaml:"rules"`
 }
